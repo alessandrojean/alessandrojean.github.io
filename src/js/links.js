de436273c5b03a8714236d283a27a00c9dfdeeba
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2022-10-06 17:54:12 -0300

    Clear the old website.

--- src/js/links.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  var links = document.links;
-  for (let i = 0; i < links.length; i++) {
-    if (links[i].hostname !== window.location.hostname) {
-      links[i].target = '_blank';
-    }
-  }
-})();
