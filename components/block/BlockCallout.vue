fff793e9febf9130587e7e91c440f7fb7a738063
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2022-10-13 18:20:43 -0300

    Add missing color to callout.

--- components/block/BlockCallout.vue
+++ components/block/BlockCallout.vue
@@ -14,12 +14,13 @@ const props = withDefaults(defineProps<NotionBlockProps>(), {
 const { block, pass, richText } = useNotionParser<CalloutBlockObjectResponse>(props)
 
 const colorMap: Partial<Record<ApiColor, string[]>> = {
-  blue_background: ['bg-blue-100', 'text-blue-800', 'border-blue-200'],
   gray_background: ['bg-gray-100', 'text-gray-800', 'border-gray-200'],
   orange_background: ['bg-orange-100', 'text-orange-900', 'border-orange-200'],
   yellow_background: ['bg-yellow-50', 'text-yellow-900', 'border-yellow-300'],
   green_background: ['bg-emerald-100', 'text-emerald-900', 'border-emerald-200'],
+  blue_background: ['bg-blue-100', 'text-blue-800', 'border-blue-200'],
   purple_background: ['bg-purple-100', 'text-purple-900', 'border-purple-200'],
+  pink_background: ['bg-pink-100', 'text-pink-900', 'border-pink-200'],
   red_background: ['bg-red-100', 'text-red-900', 'border-red-200']
 }
 
