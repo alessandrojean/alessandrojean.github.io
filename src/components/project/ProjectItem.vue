ce350a5eb5b3bf52bb799376670b01f16e2fb0b8
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2024-05-03 21:18:46 -0300

    Add support to project logos.

--- src/components/project/ProjectItem.vue
+++ src/components/project/ProjectItem.vue
@@ -3,21 +3,47 @@ import type { Project } from '@/server/api/projects.get';
 import { LinkIcon } from '@heroicons/vue/20/solid';
 
 const props = defineProps<{ project: Project }>()
+const { project } = toRefs(props)
+
+const parsedUrl = computed(() => {
+  if (!project.value.url) {
+    return null
+  }
+
+  return new URL(project.value.url)
+})
 
 const domain = computed(() => {
-  return props.project.url ? new URL(props.project.url).host : null
+  if (!parsedUrl.value) {
+    return null
+  }
+
+  return parsedUrl.value.host === 'github.com'
+    ? parsedUrl.value.pathname.substring(1)
+    : parsedUrl.value.host
 })
 
 const { locale } = useI18n({ useScope: 'global' })
+
+const logoSrc = computed(() => {
+  if (process.dev || !project.value.logo) {
+    return project.value.logo
+  }
+
+  return `/notion-img/projects/${project.value.slug}.${fileNameFromUrl(project.value.logo)}`
+})
 </script>
 
 <template>
-  <div class="group relative">
+  <div class="group relative flex flex-col items-start">
+    <div v-if="logoSrc" class="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-gray-800/5 ring-1 ring-gray-900/5 dark:border dark:border-gray-700/50 dark:bg-gray-800 dark:ring-0">
+      <NuxtImg :src="logoSrc" width="32" height="32" class="size-8 rounded-full" loading="lazy" />
+    </div>
     <component
       :is="project.url ? 'a' : 'h3'"
       :href="project.url"
       :target="project.url ? '_blank' : null"
-      class="peer font-semibold font-display-safe dark:text-gray-200 dark:group-hover:text-gray-100 dark:contrast-more:text-gray-100 motion-safe:transition focus:outline-none"
+      class="mt-6 peer font-semibold font-display-safe dark:text-gray-200 dark:group-hover:text-gray-100 dark:contrast-more:text-gray-100 motion-safe:transition focus:outline-none"
       >
       <span class="absolute -inset-4 md:-inset-6 z-20 md:rounded-2xl" />
       <span class="relative z-10">
@@ -30,7 +56,8 @@ const { locale } = useI18n({ useScope: 'global' })
     </p>
 
     <p v-if="domain" class="relative z-10 mt-6 text-gray-400 dark:text-gray-200 group-hover:text-primary-600 peer-focus-visible:text-primary-600 dark:group-hover:text-primary-500 dark:peer-focus-visible:text-primary-500 flex items-center font-semibold text-sm motion-safe:transition">
-      <LinkIcon class="size-4" aria-hidden="true" />
+      <IconGitHub v-if="parsedUrl?.host === 'github.com'" class="size-4 fill-current" aria-hidden="true" />
+      <LinkIcon v-else class="size-4" aria-hidden="true" />
       <span class="ml-3 font-medium">{{ domain }}</span>
     </p>
   </div>
--- src/server/api/projects.get.ts
+++ src/server/api/projects.get.ts
@@ -1,6 +1,8 @@
 import type { ProjectObjectResponse } from '@/lib/notion';
 import { fetchTable, getTextContent } from '@/lib/notion';
 
+import { defineExtractorEventHandler, fileNameFromUrl } from '#media-extractor';
+
 export interface ProjectCategory {
   name: Record<string, string>;
   projects: Project[]
@@ -8,6 +10,7 @@ export interface ProjectCategory {
 
 export interface Project {
   id: string;
+  logo: string | null;
   name: string;
   description: Record<string, string>;
   slug: string;
@@ -16,8 +19,9 @@ export interface Project {
   isPublic: boolean;
 }
 
-export default defineEventHandler<ProjectCategory[]>(async () => {
-  const { notionProjectsTable } = useRuntimeConfig()
+export default defineExtractorEventHandler({
+  async handler() {
+    const { notionProjectsTable } = useRuntimeConfig()
 
   const publicFilter = {
     property: 'Public',
@@ -36,8 +40,9 @@ export default defineEventHandler<ProjectCategory[]>(async () => {
   const projects = response.results as ProjectObjectResponse[]
 
   const categories: Record<string, Project[]> = projects
-    .map(({ id, properties }) => ({
+    .map(({ id, properties, icon }) => ({
       id: id,
+      logo: icon?.type === 'file' ? icon.file.url : null,
       name: getTextContent(properties['Name']['title']),
       description: {
         pt: getTextContent(properties['Description']['rich_text']),
@@ -69,4 +74,19 @@ export default defineEventHandler<ProjectCategory[]>(async () => {
       },
       projects
     }))
+  },
+
+  async extract(response) {
+    return {
+      destination: 'projects',
+      medias: response.flatMap(({ projects }) => {
+        return projects
+          .filter(({ logo }) => logo !== null)
+          .map(({ logo, slug }) => ({
+            url: logo!!,
+            fileName: `${slug}.${fileNameFromUrl(logo!!).extension}`
+          }))
+      })
+    }
+  }
 })
