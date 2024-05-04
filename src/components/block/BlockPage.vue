d4731371fbfb959ec4ddce7192dc109f4da005b7
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2024-05-04 00:42:07 -0300

    Make use of typography plugin in page.

--- src/components/block/BlockPage.vue
+++ src/components/block/BlockPage.vue
@@ -55,41 +55,43 @@ const language = computed(() => {
 </script>
 
 <template>
-  <article v-if="level === 0 && fullPage" class="notion max-w-2xl mx-auto" :lang="language">
-    <div class="flex flex-col">
-      <h1 class="notion-title mt-6 text-4xl sm:!leading-[3.5rem] font-display-safe font-bold tracking-tight text-gray-800 dark:text-gray-100 sm:text-5xl motion-safe:transition">
-        <BlockTextRenderer :text="title" v-bind="pass" />
-      </h1>
-      <div
-        v-if="description"
-        class="mt-4 prose prose-xl prose-p:text-gray-600 dark:prose-p:text-gray-400 dark:prose-invert motion-safe:transition max-w-none"
-      >
-        <p><BlockTextRenderer :text="description" v-bind="pass" /></p>
-      </div>
-      <span
-        v-if="date.iso"
-        class="order-first text-gray-500 dark:text-gray-500 motion-safe:transform border-l-2 border-gray-300 dark:border-gray-600 pl-3"
-      >
-        <time :datetime="date.iso">{{ date.formatted }}</time>
-        <template v-if="date.updatedIso && date.wasUpdated">
-          —
-          <i18n-t
-            tag="span"
-            keypath="posts.updatedAt"
-          >
-            <time :datetime="date.updatedIso">
-              {{ date.updatedFormatted }}
-            </time>
-          </i18n-t>
-        </template>
-      </span>
+  <article
+    :lang="language"
+    class="notion prose dark:prose-invert motion-safe:transition max-w-2xl mx-auto prose-h1:font-bold prose-a:text-primary-600 dark:prose-a:text-primary-400 hover:prose-a:text-primary-700 focus-visible:prose-a:text-primary-700 dark:hover:prose-a:text-primary-400 dark:focus-visible:prose-a:text-primary-400 prose-a:no-underline hover:prose-a:underline hover:prose-a:underline-offset-2 hover:prose-a:decoration-2 hover:prose-a:decoration-primary-500/80 dark:hover:prose-a:decoration-primary-400/80 dark:[&_pre>code]:bg-inherit focus:prose-a:outline-none focus-visible:prose-a:ring-2 focus-visible:prose-a:ring-offset-2 focus-visible:prose-a:ring-primary-600 dark:focus-visible:prose-a:ring-offset-gray-900 dark:contrast-more:focus-visible:prose-a:ring-offset-black prose-a:rounded prose-a:motion-safe:transition"
+  >
+    <template v-if="level === 0 && fullPage">
+      <div class="flex flex-col">
+        <h1 class="notion-title mt-5 mb-0 font-display-safe tracking-tight text-4xl sm:!text-5xl sm:!leading-[3.5rem]">
+          <BlockTextRenderer :text="title" v-bind="pass" />
+        </h1>
+        <p
+          v-if="description"
+          class="lead text-gray-600 dark:text-gray-400 mt-5"
+        >
+          <BlockTextRenderer :text="description" v-bind="pass" />
+        </p>
 
-    </div>
-    <div id="post-content" class="mt-12 prose dark:prose-invert motion-safe:transition max-w-none prose-a:text-primary-600 dark:prose-a:text-primary-400 hover:prose-a:text-primary-700 focus-visible:prose-a:text-primary-700 dark:hover:prose-a:text-primary-400 dark:focus-visible:prose-a:text-primary-400 prose-a:no-underline hover:prose-a:underline hover:prose-a:underline-offset-2 hover:prose-a:decoration-2 hover:prose-a:decoration-primary-500/80 dark:hover:prose-a:decoration-primary-400/80 dark:[&_pre>code]:bg-inherit focus:prose-a:outline-none focus-visible:prose-a:ring-2 focus-visible:prose-a:ring-offset-2 focus-visible:prose-a:ring-primary-600 dark:focus-visible:prose-a:ring-offset-gray-900 dark:contrast-more:focus-visible:prose-a:ring-offset-black prose-a:rounded prose-a:motion-safe:transition">
-      <slot />
-    </div>
-  </article>
-  <article v-else-if="level === 0" id="page-content" class="notion prose dark:prose-invert motion-safe:transition max-w-none prose-a:text-primary-600 dark:prose-a:text-primary-400 hover:prose-a:text-primary-700 focus-visible:prose-a:text-primary-700 dark:hover:prose-a:text-primary-400 dark:focus-visible:prose-a:text-primary-400 prose-a:no-underline hover:prose-a:underline hover:prose-a:underline-offset-2 hover:prose-a:decoration-2 hover:prose-a:decoration-primary-500/80 dark:hover:prose-a:decoration-primary-400/80 dark:[&_pre>code]:bg-inherit focus:prose-a:outline-none focus-visible:prose-a:ring-2 focus-visible:prose-a:ring-offset-2 focus-visible:prose-a:ring-primary-600 dark:focus-visible:prose-a:ring-offset-gray-900 dark:contrast-more:focus-visible:prose-a:ring-offset-black prose-a:rounded prose-a:motion-safe:transition prose-h1:font-bold" :lang="language">
+
+        <span
+          v-if="date.iso"
+          class="not-prose order-first text-gray-500 dark:text-gray-500 motion-safe:transform border-l-2 border-gray-300 dark:border-gray-600 pl-3"
+        >
+          <time :datetime="date.iso">{{ date.formatted }}</time>
+          <template v-if="date.updatedIso && date.wasUpdated">
+            —
+            <i18n-t
+              tag="span"
+              keypath="posts.updatedAt"
+            >
+              <time :datetime="date.updatedIso">
+                {{ date.updatedFormatted }}
+              </time>
+            </i18n-t>
+          </template>
+        </span>
+      </div>
+    </template>
+
     <slot />
   </article>
 </template>
