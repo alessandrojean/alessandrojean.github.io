bd4bbf35ff4ba1ad208cceeec677a76fbd0f9761
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2024-05-18 19:40:14 -0300

    Remove border from callout.

--- src/components/block/BlockCallout.vue
+++ src/components/block/BlockCallout.vue
@@ -14,14 +14,14 @@ const props = withDefaults(defineProps<NotionBlockProps>(), {
 const { block, pass, richText } = useNotionParser<NotionApi.CalloutBlockObjectResponse>(toRefs(props))
 
 const colorMap: { [key in ApiColor]?: string[] } = {
-  gray_background: ['bg-gray-50', 'text-gray-800', 'border-gray-100'],
-  orange_background: ['bg-orange-50', 'text-orange-900', 'border-orange-100'],
-  yellow_background: ['bg-yellow-50', 'text-yellow-900', 'border-yellow-100'],
-  green_background: ['bg-emerald-50', 'text-emerald-900', 'border-emerald-100'],
-  blue_background: ['bg-blue-50', 'text-blue-900', 'border-blue-100'],
-  purple_background: ['bg-purple-50', 'text-purple-900', 'border-purple-100'],
-  pink_background: ['bg-pink-50', 'text-pink-900', 'border-pink-100'],
-  red_background: ['bg-red-50', 'text-red-900', 'border-red-100']
+  gray_background: ['bg-gray-50', 'text-gray-800'],
+  orange_background: ['bg-orange-50', 'text-orange-900'],
+  yellow_background: ['bg-yellow-50', 'text-yellow-900'],
+  green_background: ['bg-emerald-50', 'text-emerald-900'],
+  blue_background: ['bg-blue-50', 'text-blue-900'],
+  purple_background: ['bg-purple-50', 'text-purple-900'],
+  pink_background: ['bg-pink-50', 'text-pink-900'],
+  red_background: ['bg-red-50', 'text-red-900']
 }
 
 const color = computed(() => block.value.callout.color)
@@ -36,11 +36,6 @@ const textColor = computed(() => {
     ?? colorMap.gray_background!![1]
 })
 
-const borderColor = computed(() => {
-  return colorMap[color.value]?.[2]
-    ?? colorMap.gray_background!![3]
-})
-
 const isAddress = computed(() => {
   return block.value.callout.icon?.type === 'emoji'
     && block.value.callout.icon.emoji === '🗺️'
@@ -51,10 +46,9 @@ const isAddress = computed(() => {
   <div
     :class="[
       'notion-callout my-8 dark:bg-gray-800 dark:contrast-more:bg-gray-900 dark:text-gray-300 motion-safe:transition',
-      'flex flex-col md:flex-row p-3 rounded-lg gap-3 border dark:border-gray-800 dark:contrast-more:border-gray-900',
+      'flex flex-col md:flex-row p-3 rounded-lg gap-3',
       bgColor,
       textColor,
-      borderColor
     ]"
   >
     <div class="shrink-0 ps-1" v-if="block.callout.icon">
