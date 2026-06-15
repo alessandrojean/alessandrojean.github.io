1c2b1332a7acbaeb0b3f31c8f1ba2e50165f4080
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2026-06-15 11:32:10 -0300

    feat: support cite in Tweet

--- app/components/content/Tweet.vue
+++ app/components/content/Tweet.vue
@@ -2,6 +2,7 @@
   <blockquote
     data-slot="tweet"
     class="py-4"
+    :cite="url"
   >
     <div class="relative text-lg/normal md:text-2xl/normal font-sans bg-(--bg) border border-(--border) rounded-lg px-4 py-3 [--bg:var(--color-gray-100)] dark:[--bg:var(--color-gray-800)] [--border:var(--color-gray-300)] dark:[--border:var(--color-gray-700)] after:size-5 after:bg-(--bg) after:block after:absolute after:-bottom-2.5 after:inset-s-6 lg:after:inset-s-8 after:border-b after:border-e after:border-b-(--border) after:border-e-(--border) after:rotate-45">
       <slot />
@@ -36,5 +37,6 @@ defineProps<{
   author: string;
   subtitle?: string;
   picture: string;
+  url?: string;
 }>();
 </script>
