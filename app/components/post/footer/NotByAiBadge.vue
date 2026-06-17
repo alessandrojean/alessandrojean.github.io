2114c8598ff53c3967eebf2f8f9787fc777fb124
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2026-06-17 18:18:32 -0300

    fix: show english not by AI badge

--- app/components/post/footer/NotByAiBadge.vue
+++ app/components/post/footer/NotByAiBadge.vue
@@ -6,8 +6,18 @@
     class="w-fit"
   >
     <img
+      v-if="language === 'pt-BR'"
       src="/badge-not-by-ai-pt-br.svg"
       alt="Escrito por um humano, não por IA"
     >
+    <img
+      v-else
+      src="/badge-not-by-ai-en-us.svg"
+      alt="Written by humans, not by AI"
+    >
   </NuxtLink>
 </template>
+
+<script setup lang="ts">
+const language = inject<Ref<string>>('language');
+</script>
