1859562d9f88143460d7c64e247bcc0cc438abd7
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2026-06-21 23:40:56 -0300

    refactor: reuse same link in header

--- app/components/site/Header.vue
+++ app/components/site/Header.vue
@@ -17,41 +17,25 @@
 
     <div class="flex items-center gap-6 text-gray-500 dark:text-gray-400 *:hover:text-black dark:*:hover:text-gray-200 *:data-[site=bluesky]:hidden sm:*:data-[site=bluesky]:block">
       <NuxtLink
-        class="hidden sm:block"
-        to="/about"
-      >
-        About
-      </NuxtLink>
-
-      <NuxtLink
-        class="hidden sm:block"
-        to="/blog"
-      >
-        Blog
-      </NuxtLink>
-
-      <NuxtLink
-        class="sm:hidden"
         to="/about"
         title="About"
       >
         <Icon
           name="lucide:info"
-          class="size-4.5"
+          class="size-4.5 sm:hidden"
         />
-        <span class="sr-only">About</span>
+        <span class="sr-only sm:not-sr-only">About</span>
       </NuxtLink>
 
       <NuxtLink
-        class="sm:hidden"
         to="/blog"
         title="Blog"
       >
         <Icon
           name="lucide:notebook-text"
-          class="size-4.5"
+          class="size-4.5 sm:hidden"
         />
-        <span class="sr-only">Blog</span>
+        <span class="sr-only sm:not-sr-only">Blog</span>
       </NuxtLink>
 
       <NuxtLink
